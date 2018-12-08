f4d5afdfea7ca6d5d47fcba69bf48ea00a2723e5
Judah Goff <bgoff1@harding.edu>
2018-12-08 14:36:12 -0600

    Add build to docs folder

---
+++ docs/build/precache-manifest.eff83e7e533dbe34769898ff48b10ce8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "52e6b4810ac7eeee60ec",
+    "url": "/static/js/main.52e6b481.chunk.js"
+  },
+  {
+    "revision": "9627143cbcdb44cd637e",
+    "url": "/static/js/1.9627143c.chunk.js"
+  },
+  {
+    "revision": "52e6b4810ac7eeee60ec",
+    "url": "/static/css/main.3d9e5a8b.chunk.css"
+  },
+  {
+    "revision": "a91c912f48a5b9a84bd1197a0d6a9833",
+    "url": "/index.html"
+  }
+];
