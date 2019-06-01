a42ee34e6dcb5f8e2a7380235367e0926b030fa5
Judah Goff <bgoff1@harding.edu>
2019-05-31 21:20:47 -0500

    Remove Google Analytics
    Signed-off-by: Judah Goff <bgoff1@harding.edu>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
 import Page from "./components/Page";
@@ -7,16 +7,12 @@ import createStore from './createStore';
 
 const { store, persistor } = createStore();
 
-class App extends Component {
-    render() {
-        return (
-            <Provider store={ store }>
-                <PersistGate loading={ null } persistor={persistor}>
-                    <Page/>
-                </PersistGate>
-            </Provider>
-        );
-    }
-}
+const App = () => (
+    <Provider store={ store }>
+        <PersistGate loading={ null } persistor={ persistor }>
+            <Page/>
+        </PersistGate>
+    </Provider>
+);
 
 export default App;
