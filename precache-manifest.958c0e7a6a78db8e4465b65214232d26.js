f2a1823d13c3fa4e50c36b8409eebd475ba0884b
Judah Goff <bgoff1@harding.edu>
2019-10-28 10:03:56 -0500

    Updates

---
+++ precache-manifest.958c0e7a6a78db8e4465b65214232d26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7193173f0838acf1a719",
+    "url": "/pokemon-react/static/js/runtime~main.2a83345f.js"
+  },
+  {
+    "revision": "bb1934135ccecfa6bbd2",
+    "url": "/pokemon-react/static/js/main.137838ea.chunk.js"
+  },
+  {
+    "revision": "5715090a7d0dcff113ab",
+    "url": "/pokemon-react/static/js/2.422e6f8d.chunk.js"
+  },
+  {
+    "revision": "bb1934135ccecfa6bbd2",
+    "url": "/pokemon-react/static/css/main.b8cdbe44.chunk.css"
+  },
+  {
+    "revision": "3812131a0e6e175d9a2d49d6cc043734",
+    "url": "/pokemon-react/index.html"
+  }
+];
